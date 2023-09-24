fe7b5a1252daad3ece8114421059363bc363eea4
NursultanFront <nursultan020395@gmail.com>
2023-09-24 14:47:54 +0500

    feat: grand finale

--- src/js/main.js
+++ src/js/main.js
@@ -33,4 +33,49 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   }
+
+  const asideNav = document.querySelector(".vehicle__aside.aside-nav");
+  const stickyPoint = 400; // например, после прокрутки на 200px
+
+  if (asideNav) {
+    window.addEventListener("scroll", function () {
+      if (window.scrollY >= stickyPoint) {
+        asideNav.classList.add("sticky");
+      } else {
+        asideNav.classList.remove("sticky");
+      }
+    });
+  }
+
+  const sections = [
+    document.querySelector("#descr"),
+    document.querySelector("#char"),
+    document.querySelector("#gallery"),
+  ];
+
+  const navItems = document.querySelectorAll(".aside-nav__item");
+
+  const PADDING = 60;
+
+  if (navItems && navItems.length !== 0) {
+    window.addEventListener("scroll", function () {
+      sections.forEach((section, index) => {
+        const rect = section.getBoundingClientRect();
+        const nextSection = sections[index + 1];
+        const nextRect = nextSection
+          ? nextSection.getBoundingClientRect()
+          : null;
+
+        if (
+          rect.top <= PADDING &&
+          (!nextRect || (nextRect && nextRect.top > PADDING))
+        ) {
+          navItems.forEach((item) =>
+            item.classList.remove("aside-nav__item--active")
+          );
+          navItems[index].classList.add("aside-nav__item--active");
+        }
+      });
+    });
+  }
 });
